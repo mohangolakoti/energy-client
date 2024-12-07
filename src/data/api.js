af3f1c53da4a8b87324cda39699bebd6f49387be
Mohan <ramgolakoti1@gmail.com>
2024-12-07 22:53:08 +0530

    api url

--- src/data/api.js
+++ src/data/api.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://65.1.134.192:5000/api/sensordata1"
-export const API_URL2 = "http://65.1.134.192:5000/api1"
+export const API_URL = "http://13.201.229.45:5000/api/sensordata1"
+export const API_URL2 = "http://13.201.229.45:5000/api1"
 //export const API_URL = "http://localhost:5000/api/sensordata1"
 //export const API_URL2 = "http://localhost:5000/api1"
