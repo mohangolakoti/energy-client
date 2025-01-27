b90b387abfcfe747fbbf50957b4e9722151978da
Mohan <ramgolakoti1@gmail.com>
2025-01-27 21:39:48 +0530

    new instance ip

--- src/data/api.js
+++ src/data/api.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://65.0.95.129:5000/api/sensordata1"
-export const API_URL2 = "http://65.0.95.129:5000/api1"
+export const API_URL = "http://52.66.189.51:4000/api/sensordata1"
+export const API_URL2 = "http://52.66.189.51:4000/api1"
 //export const API_URL = "http://localhost:5000/api/sensordata1"
 //export const API_URL2 = "http://localhost:5000/api1"
