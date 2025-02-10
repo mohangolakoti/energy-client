7dfcf5d40549d81d75c72063ae9465d85419719d
Mohan <ramgolakoti1@gmail.com>
2025-02-10 10:34:42 +0530

    link

--- src/data/api.js
+++ src/data/api.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://52.66.189.51:4000/api/sensordata1"
-export const API_URL2 = "http://52.66.189.51:4000/api1"
+export const API_URL = "https://www.radhakrishnaricemill.gfiotsolutions.com/api/sensordata1"
+export const API_URL2 = "https://www.radhakrishnaricemill.gfiotsolutions.com/api1"
 /* export const API_URL = "http://localhost:4000/api/sensordata1"
 export const API_URL2 = "http://localhost:4000/api1" */
